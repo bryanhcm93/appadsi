b3e90886c2865e73da251bf0be5f05b22343b45c
bryan <bryanhcmrap@gmail.com>
2021-11-03 08:50:57 -0500

    registar,actualizar,eliminar

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -1,10 +1,46 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\categoria;
 
 use Illuminate\Http\Request;
+
 
 class CategoriaController extends Controller
 {
     //
+public function index(){
+
+
+    $categoria=categoria::all();
+    return ['cat'=>$categoria];
+}
+
+
+
+    public function store(Request $request){
+
+        $categoria= new categoria();
+        $categoria->nombre= $request->nombre;
+        $categoria->edo= $request->edo;
+
+        $categoria->save();
+
+
+
+    }
+
+    public function update(Request $request ){
+$categoria=Categoria::findOrFail($request->id);
+$categoria->nombre= $request->nombre;
+$categoria->edo= $request->edo;
+$categoria->save();
+
+    }
+
+    public function destroy(Request $request){
+        $categoria=Categoria::findOrFail($request->id);
+        $categoria->delete();
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\CategoriaController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+//////////////////// routes categoria////////
+Route::get('/api/categoria', [CategoriaController::class, 'index']);
+Route::post('/api/categoria/registrar', [CategoriaController::class, 'store']);
+Route::put('/api/categoria/actualizar', [CategoriaController::class, 'update']);
+Route::post('/api/categoria/eliminar', [CategoriaController::class, 'destroy']);
